822c56fd98b52acf91fb4cc700ba340b09a05389
nvpinkham <47755049+nvpinkham@users.noreply.github.com>
2022-06-23 10:33:20 -0600

    Add files via upload

---
+++ Figure_3/Figure_3_Panel_A.22.06.22.R
@@ -0,0 +1,67 @@
+
+library(vegan)
+
+
+map <- read.csv("data/ASD_map.csv",
+                header=TRUE,
+                row.names=1)
+
+map$sample.id <- paste0(map$mouse,
+                        ".",
+                        substr(map$genotype, 1, 1),
+                        ".",
+                        map$time)
+
+otu <- read.csv("data/FD_OTU_mice21.csv",
+                row.names = 1,
+                header = T)
+
+
+otu <- otu[match(map$sample.id, row.names(otu)),]
+
+######################### FINISH MAP ############################
+# mice were separated by sex and genotype
+map$cage <- paste0(map$genotype, ".", map$sex)
+
+map$invsimp <- diversity(otu, "invsimp")
+m.dist <- vegdist(otu)
+map$discription <- paste("day", map$time, map$genotype)
+
+map$color[map$genotype == "mutant"] <-  "#D55E00"
+map$color[map$genotype != "mutant"] <-  "#F0E442"
+
+
+par(mfrow=c(1, 3))
+par(mar=c(4.1, 4.1, 4.1, 1))
+
+time.pick <- c(0, 25, 54)
+
+for(i in 1 : length(time.pick)){
+
+  now <- time.pick[i]
+
+  otu.pick <- otu[map$time == now , ]
+  map.pick <- map[map$time == now , ]
+
+  set.seed(42)
+  otu.nmds <- metaMDS(otu.pick, plot = F,  k = 2, try = 20)
+
+  {plot(otu.nmds$points,
+        main = paste("Day", time.pick[i],
+                     "\nn =", nrow(otu.pick)))
+
+    ordispider(otu.nmds$points, groups = map.pick$discription,
+               col = 8, lty = 1)
+    points(otu.nmds$points, pch = 21, bg = map.pick$color, cex = 2)
+
+    if(now == 0){
+      legend("bottomleft",
+             pch = 21,
+             cex = 1.5,
+             pt.bg = c(  map.pick[1,]$color,
+                         map.pick[2,]$color),
+             legend = c(  map.pick[1,]$genotype,
+                          map.pick[2,]$genotype))
+    }
+  }
+}
