770e6bb31dcf7918b697a6eb9c9cc26f963cf967
nvpinkham <47755049+nvpinkham@users.noreply.github.com>
2022-06-27 15:06:32 -0600

    Add files via upload

---
+++ Figure_3/Figure_3_Panel_D.22.06.27.R
@@ -0,0 +1,40 @@
+
+library(vegan)
+library(labdsv)
+library(RColorBrewer)
+
+source("R/mice_functions.22.06.22.R")
+
+rs <-list.files("MetaboAnalystR-master/R", pattern = ".R", full.names = T)
+
+for(i in rs[-1]){
+ source(i)
+}
+
+
+dir.create("processing")
+
+meta.6 <-  t(read.csv("data/6mo2018mousestool_umolepermg.csv",
+                      header = T, row.names = 1))
+meta.6 <- as.data.frame(meta.6)
+
+meta.6 <- norm(row.names(meta.6), meta.6)
+
+#########
+
+p6 <- meta.6$Label
+
+meta.6$Label <- NULL
+
+adonis(dist(meta.6) ~ p6)
+
+map.6 <- as.data.frame(cbind(row.names(meta.6),
+                             p6,
+                             as.numeric(as.factor(p6))))
+colnames(map.6) <- c("id", "discription", "col")
+
+map.6$col[map.6$discription == "FD_6mo"] <-  "#D55E00"
+map.6$col[map.6$discription != "FD_6mo"] <-  "#F0E442"
+
+plot.meta(meta.pick = meta.6, map.pick = map.6)
+
