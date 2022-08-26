476861b4c9d976edd0a20c48c0bd89631c14f420
Nick Pinkham <47755049+nvpinkham@users.noreply.github.com>
2022-08-26 12:31:20 -0600

    Update human_functions.22.06.18.R

--- R/human_functions.22.06.18.R
+++ R/human_functions.22.06.18.R
@@ -782,3 +782,57 @@ get.used <- function(map, pick = "Stool 16S"){
   return(res)
 }
 
+venn2otu <- function(otu, map, 
+                     group1 = "mouse gut metagenome", 
+                     group2 = "human gut metagenome", 
+                     min.counts = 10, 
+                     min.sites = 5){
+
+  if(!"split" %in% colnames(map)){
+    message('need col named "split"')
+  }
+  
+  if(!all(row.names(map) == row.names(otu))){
+    return('CHECK that samples match')
+  }
+  
+  otu.g1 <- otu[map$split == group1, ]
+  otu.g2 <- otu[map$split == group2, ]
+  
+  aa <- min(c(nrow(otu.g1),
+              nrow(otu.g2)))
+  
+  set.seed(42)
+  otu.g1 <- otu.g1[sample(1:nrow(otu.g1), aa),]
+  otu.g2 <- otu.g2[sample(1:nrow(otu.g2), aa),]
+  
+  g1 <- colSums(otu.g1)
+  g2 <- colSums(otu.g2)
+  
+  g1.pa <- colSums(otu.g1 > 0)
+  g2.pa <- colSums(otu.g2 > 0)
+  
+  unique2g1 <- g1.pa > min.sites & g2.pa < min.sites
+  unique2g2 <- g2.pa > min.sites & g1.pa < min.sites
+  
+  shared <- g1.pa >= min.sites & g2.pa >= min.sites
+  
+  sum(otu[ , shared])
+  sum(otu[ , unique2g1])
+  sum(otu[ , unique2g2])
+  
+  grid::grid.newpage()
+  VennDiagram::draw.pairwise.venn(area1=length(unique2g1[unique2g1]), 
+                     area2=length(unique2g2[unique2g2]),
+                     cross.area=length(shared[ shared]),
+                     category=c(group1, group2),
+                     fill=c("Red","Yellow"))
+  
+  g1.proportion <- sum(otu.g1[ , shared]) /sum(otu.g1)
+  g2.proportion <- sum(otu.g2[ , shared]) /sum(otu.g2)
+  
+  res <- c(g1.proportion, g2.proportion)
+  names(res) <- c(group1, group2)
+  
+  return(res)
+}
